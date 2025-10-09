fe1b96ae074efacb365f7342bf8257a41fd04a24
Nuzul Abatony <78489357+Albtony@users.noreply.github.com>
2025-10-09 08:55:20 +0700

    add write up

--- LeetCode/CPP/3Sum Closest (Medium).cpp
+++ LeetCode/CPP/3Sum Closest (Medium).cpp
@@ -1,3 +1,28 @@
+/*
+Source: https://leetcode.com/problems/3sum-closest/description/
+Diff: Medium
+
+Given an integer array nums of length n and an integer target, find three integers in nums 
+such that the sum is closest to target.
+Return the sum of the three integers.
+You may assume that each input would have exactly one solution.
+
+Example 1:
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+Example 2:
+Input: nums = [0,0,0], target = 1
+Output: 0
+Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+
+Constraints:
+3 <= nums.length <= 500
+-1000 <= nums[i] <= 1000
+-104 <= target <= 104
+*/
+
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
@@ -43,3 +68,17 @@ int main() {
 
     return 0;
 }
+
+/*
+its a modification of 3 sum
+
+instead of finding a collections of qualified triplets, you just want to know the closest
+sum your triplets can get to target
+
+so the main idea stays the same, using 1 + 2 pointers to traverse the list
+but its slightly easier since you dont have to mind duplicates
+
+and since perfectly equal is the best you can get, you can just return it when you find it
+instead of doing extra work to avoid duplicates, so just wire things together again
+track the best, and return it!
+*/
